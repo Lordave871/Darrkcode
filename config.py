f14ff64d673a7bd4dd9d9df8ec19558db34a8c43
Lordave871 <nwahiridaviduche@gmail.com>
2026-04-10 04:07:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
 class Config:
-    DATABASE_NAME=os.getenv('DATABASE_NAME')
-    DATABASE_HOST=os.getenv('DATABASE_HOST')
-    DATABASE_USER=os.getenv('DATABASE_USER')
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_USER = os.getenv('DATABASE_USER')
     DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-    DATABASE_PORT=(os.getenv('DATABASE_PORT'))
-    AT_API_KEY=os.getenv('AT_API_KEY')
+    
+    # Force the port to be an integer here
+    # We use 13757 as the default based on your .env
+    DATABASE_PORT = int(os.getenv('DATABASE_PORT', 13757)) 
 
-
-    SECRET_KEY=os.getenv('SECRET_KEY','default_secrete_key')
-    
-    
-    
+    AT_API_KEY = os.getenv('AT_API_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
